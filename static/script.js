16ef9db99fdb56604c8df0ebf2148d63fc33df8e
xueying liu <xueyingliu@Mac.attlocal.net>
2025-02-18 22:17:02 -0500

    change api path

--- static/script.js
+++ static/script.js
@@ -10,7 +10,7 @@ function sendMessage() {
     addMessage(message, "user-message");
     input.value = "";
 
-    fetch("http://127.0.0.1:5000/chat", {
+    fetch("/chat", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
