b1f9b9bbf2c2c485585b04c3d103ea48c4d57652
xueying2976 <xliu41@tufts.edu>
2025-03-09 12:52:52 -0400

    follow-up question3.0

--- app.py
+++ app.py
@@ -52,12 +52,7 @@ def main():
             response = {
                 "text": f"""
                         Hello, I can help you with:
-                        - Course Information:
-                          I would like to understand the [degree/program name] requirements for the [undergraduate/graduate/PhD] program, including:
-		                    • Total credit requirements
-	                        • Core course requirements
-	                        • Minimum credits needed for electives
-                       Can you provide detailed information?
+                        - Course Information 3.0:
                         - Program Requirements
                         - Course Planning
                         - CS Department Contact
@@ -93,11 +88,6 @@ def main():
             "text": f"""
                     I can help you with:
                     - Course Information
-                      I would like to understand the [degree/program name] requirements for the [undergraduate/graduate/PhD] program, including:
-		                • Total credit requirements
-	                    • Core course requirements
-	                    • Minimum credits needed for electives
-                       Can you provide detailed information?
                     - Program Requirements
                     - Course Planning
                     - CS Department Contact
